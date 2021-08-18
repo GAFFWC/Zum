55bdd65b1e4bd3e9308bc006d5046c0dc12c3004
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-08-19 02:13:22 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import http from "http";
-import socketio from "socket.io";
+import { Server } from "socket.io";
 import pug from "pug";
+import { instrument } from "@socket.io/admin-ui";
 const app = express();
 
 //view engine
@@ -10,48 +11,41 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 // home
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = new socketio.Server(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true
+    }
+});
 
-// wsServer.listen(httpServer, () => {
-//     console.log("Websocket Server Listening on ws://localhost:3000");
-// });
+instrument(wsServer, {
+    auth: false
+});
 
 httpServer.listen(3000, () => {
     console.log("Http/Websocket Server Listening on http://localhost:3000");
 });
 
 wsServer.on("connection", (socket) => {
-    console.log("SOCKET - ", socket.id, socket.connected ? "CONNECTED" : "DISCONNECTED");
-
-    socket["nickname"] = "unknown";
-
-    socket.onAny((event) => {
-        console.log("New Event : ", event);
-    });
-
-    socket.on("enter_room", (roomName, nickname, done) => {
-        socket.nickname = nickname;
-        socket.join(roomName);
-        done(roomName);
-        console.log("SOCKET - ", socket.id, "Joined Room", roomName);
-        socket.to(roomName).emit("welcome", socket.nickname);
-    });
-
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-
-    socket.on("new_message", (message, roomName, done) => {
-        socket.to(roomName).emit("new_message", `${socket.nickname}: ${message}`);
-        done();
-    });
-
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
-    });
+    socket
+        .on("join_room", (roomName, done) => {
+            socket.join(roomName);
+            socket.to(roomName).emit("welcome");
+        })
+        .on("offer", (offer, roomName) => {
+            socket.to(roomName).emit("offer", offer);
+        })
+        .on("answer", (answer, roomName) => {
+            socket.to(roomName).emit("answer", answer);
+        })
+        .on("ice", (ice, roomName) => {
+            socket.to(roomName).emit("ice", ice);
+        });
 });
